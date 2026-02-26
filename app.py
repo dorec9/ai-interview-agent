39a7c880053c2503517b2f18b330d312670ba66d
dorec9 <ehgus99052600@gmail.com>
2026-02-26 14:13:57 +0900

    feat: app.py 추가

---
+++ app.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import numpy as np
+import os
+import ast
+import fitz  # PyMuPDF
+from docx import Document
+import random
+import openai
+from dotenv import load_dotenv
+load_dotenv()
+import warnings
+import random
+warnings.filterwarnings("ignore", category=DeprecationWarning)
